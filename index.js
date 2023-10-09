72be4ecaaef9661e3116f46b3ca07eab232015e6
unknown <viptechmaster@gmail.com>
2023-10-09 14:12:13 +0600

    mongo connected

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//JW8GrHSrGxbBsabG
+
+
+const uri = "mongodb+srv://jewelry-house:JW8GrHSrGxbBsabG@cluster0.ildctqt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jewelryCollections = client.db("jewelryDB").collection('jewelry');
+    const userCollections = client.db("jewelryDB").collection('users');
+
+    app.post('/jewelry', async(req, res) =>{
+        const jewelry = req.body;
+        const result = await jewelryCollections.insertOne(jewelry);
+        res.send(result);
+    })
+
+    app.post('/users', async(req, res) =>{
+        const users = req.body;
+        const result = await userCollections.insertOne(users);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) =>{
+    res.send('Jewelry House Is Running');
+})
+
+app.listen(port, () =>{
+    console.log(`Jewelry House Is Running On Port ${port}`)
+})
